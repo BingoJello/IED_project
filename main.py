39903b5bcebe02d9b3b6e802c67aa7b31f6424e7
Arthur Mimouni <arthur.mimouni@gmail.com>
2022-04-24 00:37:34 +0200

    mediateur + REST

--- main.py
+++ main.py
@@ -13,14 +13,12 @@ films_spark = spark.requestFilmByTitle("Godzilla")
 for film in films_spark :
     print(film.get_name(), film.get_year(), film.get_directors(), film.get_producers(), film.get_actors())
 
-
-print("\n----Fusion----")
-
 films_request = []
+
 for film in films_db :
     films_request.append(film)
 
-if len(films_spark) > 2:
+if len(films_spark) > 1:
     films_to_insert = []
     for film_spark in films_spark :
         exist_db = False
@@ -30,6 +28,7 @@ if len(films_spark) > 2:
                 film_request.set_directors(film_spark.get_directors())
                 film_request.set_actors(film_spark.get_actors())
                 film_request.set_producers(film_spark.get_producers())
+                film_request.set_year(film_spark.get_year())
                 exist_db = True
                 break
         if exist_db == False:
@@ -42,16 +41,26 @@ if len(films_spark) > 2:
             film.set_directors(film_spark.get_directors())
             film.set_actors(film_spark.get_actors())
             film.set_producers(film_spark.get_producers())
+            film.set_year(film_spark.get_year())
             films_request.append(film)
 else:
-    index = 0
-    for film_request in films_request :
-        film_request.set_directors(films_spark[0].get_directors())
-        film_request.set_actors(films_spark[0].get_actors())
-        film_request.set_producers(films_spark[0].get_producers())
-    index += 1
-#ClientREST.getRequest("Insidious")
+    #----------------------A TRAITER-------------------
+    #films_spark[0].get_name()
+    if len(films_request) > 0 and len(films_spark) > 0:
+        for film_request in films_request :
+            if film_request.get_name() == "":
+                film_request.set_name(films_spark[0].get_name())
+            film_request.set_directors(films_spark[0].get_directors())
+            film_request.set_actors(films_spark[0].get_actors())
+            film_request.set_producers(films_spark[0].get_producers())
+    elif len(films_spark) > 0:
+        films_request.append(films_spark[0])
+
+
+print("\n----Film REST----")
+films_request = ClientREST.getRequestByFilm("Godzilla", films_request)
 
 
 for film in films_request :
-    print(film.get_name(), film.get_release_date(), film.get_worldwide_gross(), film.get_directors(), film.get_producers(), film.get_actors())
+    print(film.get_name(), film.get_year(), film.get_release_date(), film.get_worldwide_gross(), film.get_directors(), film.get_producers(), film.get_actors(),
+          film.get_genre(), film.get_plot())
--- service/ClientREST.py
+++ service/ClientREST.py
@@ -3,14 +3,105 @@ import xml.etree.ElementTree as ET
 from lxml import etree
 from xml.dom import minidom
 from xml.dom.minidom import parse
+from model.Film import Film
 
 
-def getRequest(title):
-    response = requests.get("http://www.omdbapi.com/?r=xml&i=tt3896198&apikey=a834ebc0&t="+title)
+def getRequestByFilm(title, films):
+    year = False
+    films_request = []
+
+    if len(films) > 2 :
+        for film in films:
+            genre = False
+            directors = False
+            actors = False
+
+            if film.get_year() != "":
+                year = film.get_year()
+            else:
+                year = film.get_release_date().split("/")[2]
+
+            if film.get_genre() == "":
+                genre = True
+            if len(film.get_directors()) <= 0:
+                directors = True
+            if len(film.get_actors()) <= 0:
+                actors = True
+            film_response = getResponseByFilm(title, year, genre, directors, actors)
+
+            if film_response['plot'] != "":
+                film.set_plot(film_response['plot'])
+            if len(film_response['directors']) > 0 :
+                film.set_directors(film_response['directors'])
+            if len(film_response['actors']) > 0 :
+                film.set_actors(film_response['actors'])
+            if film_response['genre'] != "" :
+                film.set_genre(film_response['genre'])
+
+            films_request.append(film)
+    else:
+        if len(films) > 0:
+            film = films[0]
+        else:
+            film = Film()
+
+        genre = False
+        directors = False
+        actors = False
+
+        if film.get_release_date() != "":
+            year = film.get_release_date().split("/")[2]
+        else:
+            year = ""
+
+        if film.get_genre() == "" :
+            genre = True
+        if len(film.get_directors()) <= 0 :
+            directors = True
+        if len(film.get_actors()) <= 0 :
+            actors = True
+        film_response = getResponseByFilm(title, year, genre, directors, actors)
+
+        if film_response is None :
+            return films_request
+
+        if film_response['plot'] != "" :
+            film.set_plot(film_response['plot'])
+        if len(film_response['directors']) > 0 :
+            film.set_directors(film_response['directors'])
+        if len(film_response['actors']) > 0 :
+            film.set_actors(film_response['actors'])
+        if film_response['genre'] != "" :
+            film.set_genre(film_response['genre'])
+        films_request.append(film)
+    return films_request
+def getResponseByFilm(title, year, genre, directors, actors):
+    key = 'i=tt3896198&apikey=a834ebc0'
+    request = "http://www.omdbapi.com/?r=xml&"+key+"&t="+title
+
+    if year != False:
+        request = request+"&y="+year
+    response = requests.get(request)
     root = ET.fromstring(response.content)
 
     for elem in root.findall('.//movie') :
-        # How to make decisions based on attributes even in 2.6:
-        return elem.attrib.get('plot')
+        if genre != False:
+            elem_genre = elem.attrib.get('genre')
+        else:
+            elem_genre = ""
+        if directors != False:
+            elem_directors = elem.attrib.get('director').split(",")
+        else:
+            elem_directors = ""
+        if actors != False:
+            elem_actors = elem.attrib.get('actors').split(",")
+        else:
+            elem_actors = ""
+
+        elem_plot = elem.attrib.get('plot')
+
+        film = {'genre' : elem_genre, 'directors' : elem_directors, 'actors' : elem_actors, 'plot' : elem_plot}
+
+        return film
 
 
